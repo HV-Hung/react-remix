b3c8d650be2b7d19e8d3f73ee679dc145724b5ed
Hoang Van Hung <hoangvanhung6876@gmail.com>
2024-08-02 16:21:49 +0700

    test on pr

--- install_gh_cli.sh
+++ install_gh_cli.sh
@@ -5,5 +5,3 @@
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 && sudo apt update \
 && sudo apt install gh -y
-alll
-heelo
